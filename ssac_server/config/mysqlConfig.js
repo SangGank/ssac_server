e41cf680820a6e7efbc90d6ae99b5a26a7fd9b4b
name234532 <89058195+name234532@users.noreply.github.com>
2021-09-18 11:10:07 +0900

    Delete ssac_server/config directory

--- ssac_server/config/mysqlConfig.js
+++
@@ -1,9 +0,0 @@
-const mysqlConfig = {
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "zz785126",
-  database: "ssac_db",
-  connectionLimit: 10,
-};
-module.exports = mysqlConfig;
